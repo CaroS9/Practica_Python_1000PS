8b942172c2299b099fe5d7de8eb81d631dad8f46
CaroS9 <91704975+CaroS9@users.noreply.github.com>
2022-10-09 19:13:49 -0300

    Update Solucio_caro.py

--- Solucio_caro.py
+++ Solucio_caro.py
@@ -1,1 +1,11 @@
-print("Hola")
+"""Crea un función “ConvertirEspaciado”, que reciba como parámetro un texto y devuelve una cadena con un espacio adicional tras cada letra. Por ejemplo,
+“Hola, tú” devolverá “H o l a , t ú “. Crea un programa principal donde se use dicha función."""
+
+def convertirEspaciado():
+  texto=input("Ingresa un texto: ")
+  texto=list(texto)
+  for i in range(1,len(texto)+1,2):
+    texto.append(" ")
+    
+  return print(texto)
+# NO SE QUE FUNCION USAR !!
